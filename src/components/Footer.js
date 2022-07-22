02fdf0fa16bc405166b4194c82539ec39a85d33d
amaar017 <109745198+amaar017@users.noreply.github.com>
2022-07-21 20:16:17 -0400

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "../styles/Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      
+      <p> Sports News.</p>
+    </div>
+  );
+}
+
+export default Footer;
